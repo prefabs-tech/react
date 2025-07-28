3b4f1f85a9a25117f8933fe5cb70c0651dff5c19
Bishal Shrestha <bishal.shrestha@dzangolab.com>
2025-07-28 10:58:47 +0545

    feat: add example using size component

--- apps/demo/src/Views/Ui/components/ModalDemo.tsx
+++ apps/demo/src/Views/Ui/components/ModalDemo.tsx
@@ -3,7 +3,7 @@ import { Button, Modal, Page } from "@prefabs.tech/react-ui";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Section } from "../../../components/Demo";
+import { CodeBlock, Section } from "../../../components/Demo";
 
 const Header = () => {
   const [t] = useTranslation("ui");
@@ -15,9 +15,13 @@ export const ModalDemo = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isOpen2, setIsOpen2] = useState<boolean>(false);
   const [isOpen3, setIsOpen3] = useState<boolean>(false);
+  const [isOpen4, setIsOpen4] = useState<boolean>(false);
 
   const [t] = useTranslation("ui");
   const navigate = useNavigate();
+
+  const content =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
   return (
     <Page
@@ -31,40 +35,104 @@ export const ModalDemo = () => {
         />
       }
     >
+      <Section title={t("headers.usage")}>
+        <p>{t("common.usage", { component: "Modal" })}</p>
+        <CodeBlock exampleCode='import { Modal } from "@prefabs.tech/react-ui"' />
+      </Section>
+
       <Section title={t("modal.usage.withoutHeader")}>
-        <Button
-          label={t("modal.showButton")}
-          onClick={() => setIsOpen(true)}
-        ></Button>
+        <Button label="Open modal" onClick={() => setIsOpen(true)}></Button>
         <Modal onHide={() => setIsOpen(false)} visible={isOpen}>
-          <p style={{ lineHeight: 1.6 }}>{t("modal.content")}</p>
+          <p style={{ lineHeight: 1.6 }}>{content}</p>
         </Modal>
+        <CodeBlock
+          exampleCode='const [isOpen, setIsOpen] = useState<boolean>(false);
+
+<Button
+  label="Open modal"
+  onClick={() => setIsOpen(true)}
+  ></Button>
+<Modal onHide={() => setIsOpen(false)} visible={isOpen}>
+  <p style={{ lineHeight: 1.6 }}>Lorem ipsum ...</p>
+</Modal>'
+        />
       </Section>
       <Section title={t("modal.usage.withHeader")}>
-        <Button
-          label={t("modal.showButton")}
-          onClick={() => setIsOpen2(true)}
-        ></Button>
+        <Button label="Open modal" onClick={() => setIsOpen2(true)}></Button>
         <Modal
           header={t("modal.header")}
           onHide={() => setIsOpen2(false)}
           visible={isOpen2}
         >
-          <p style={{ lineHeight: 1.6 }}>{t("modal.content")}</p>
+          <p style={{ lineHeight: 1.6 }}>{content}</p>
         </Modal>
+        <CodeBlock
+          exampleCode='const [isOpen, setIsOpen] = useState<boolean>(false);
+
+<Button
+  label="Open modal"
+  onClick={() => setIsOpen(true)}
+  ></Button>
+<Modal
+  header={t("modal.header")}
+  onHide={() => setIsOpen(false)}
+  visible={isOpen}
+>
+  <p style={{ lineHeight: 1.6 }}>Lorem ipsum ...</p>
+</Modal>'
+        />
       </Section>
       <Section title={t("modal.usage.headerAsFC")}>
-        <Button
-          label={t("modal.showButton")}
-          onClick={() => setIsOpen3(true)}
-        ></Button>
+        <Button label="Open modal" onClick={() => setIsOpen3(true)}></Button>
         <Modal
           header={<Header />}
           onHide={() => setIsOpen3(false)}
           visible={isOpen3}
         >
-          <p style={{ lineHeight: 1.6 }}>{t("modal.content")}</p>
+          <p style={{ lineHeight: 1.6 }}>{content}</p>
         </Modal>
+        <CodeBlock
+          exampleCode='const [isOpen, setIsOpen] = useState<boolean>(false);
+
+<Button
+  label="Open modal"
+  onClick={() => setIsOpen(true)}
+  ></Button>
+<Modal
+  header={<Header />}
+  onHide={() => setIsOpen(false)}
+  visible={isOpen}
+>
+  <p style={{ lineHeight: 1.6 }}>Lorem ipsum ...</p>
+</Modal>'
+        />
+      </Section>
+      <Section title={t("modal.usage.size")}>
+        <Button label="Open modal" onClick={() => setIsOpen4(true)}></Button>
+        <Modal
+          header={<Header />}
+          onHide={() => setIsOpen4(false)}
+          size="large"
+          visible={isOpen4}
+        >
+          <p style={{ lineHeight: 1.6 }}>{content}</p>
+        </Modal>
+        <CodeBlock
+          exampleCode='const [isOpen, setIsOpen] = useState<boolean>(false);
+
+<Button
+  label="Open modal"
+  onClick={() => setIsOpen(true)}
+  ></Button>
+<Modal
+  header={<Header />}
+  onHide={() => setIsOpen(false)}
+  size="large"
+  visible={isOpen}
+>
+  <p style={{ lineHeight: 1.6 }}>Lorem ipsum ...</p>
+</Modal>'
+        />
       </Section>
     </Page>
   );
