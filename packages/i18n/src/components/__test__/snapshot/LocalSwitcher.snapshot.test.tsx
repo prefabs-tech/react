59c4e8053cba117cd69ca74f43f4d702ffeca9e7
Bishal Shrestha <bishal.shrestha@dzangolab.com>
2025-08-11 17:20:36 +0545

    test: update test

--- packages/i18n/src/components/__test__/snapshot/LocalSwitcher.snapshot.test.tsx
+++ packages/i18n/src/components/__test__/snapshot/LocalSwitcher.snapshot.test.tsx
@@ -1,8 +1,37 @@
+import { configContext } from "@prefabs.tech/react-config";
 import { render } from "@testing-library/react";
 import { expect, test } from "vitest";
 
 import i18n from "../../../index";
 import LocaleSwitcher from "../../LocaleSwitcher";
+
+import type { AppConfig } from "@prefabs.tech/react-config";
+
+const appConfig: AppConfig = {
+  apiBaseUrl: "/",
+  appPort: "20072",
+  appTitle: "Prefabs.tech Skeletons",
+  appVersion: "0.0.1",
+  features: {
+    showVersion: true,
+  },
+  i18n: {
+    appendNamespaceToCIMode: true,
+    debug: true,
+    defaultNS: "app",
+    fallbackLng: "en",
+    supportedLngs: ["en", "fr"],
+    react: {
+      useSuspense: false,
+    },
+    resources: { en: {}, fr: {} },
+  },
+  websiteDomain: "//",
+  copyright: {
+    holder: "Prefabs.tech",
+    url: "www.prefabs-tech.com",
+  },
+};
 
 test("Component matches snapshot", () => {
   const i18nConfig = {
@@ -23,6 +52,12 @@ test("Component matches snapshot", () => {
   };
 
   i18n(i18nConfig);
-  const { container } = render(<LocaleSwitcher />);
+
+  const { container } = render(
+    <configContext.Provider value={appConfig}>
+      <LocaleSwitcher />
+    </configContext.Provider>,
+  );
+
   expect(container).toMatchSnapshot();
 });
