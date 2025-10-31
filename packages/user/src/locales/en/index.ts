c50a851d0ae78b1bf32d17e987839b94e70c3714
Suvash Vishowkarma <suvash@dzangolab.com>
2025-10-31 18:23:59 +0545

    feat(user): register default translations

---
+++ packages/user/src/locales/en/index.ts
@@ -0,0 +1,9 @@
+import invitations from "./invitations.json";
+import user from "./user.json";
+import users from "./users.json";
+
+export const en = {
+  invitations,
+  user,
+  users,
+};
---
+++ packages/user/src/locales/fr/index.ts
@@ -0,0 +1,9 @@
+import invitations from "./invitations.json";
+import user from "./user.json";
+import users from "./users.json";
+
+export const fr = {
+  invitations,
+  user,
+  users,
+};
