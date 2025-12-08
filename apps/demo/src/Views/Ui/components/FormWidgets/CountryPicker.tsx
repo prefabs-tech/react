3fee5d78edb598fc68c295fbb36b9a3677eb5b39
Utsav Luintel <utsav.luintel@dzangolab.com>
2025-12-08 17:00:00 +0545

    feat(ui/CountryPicker): add value selection to countryPicker

--- apps/demo/src/Views/Ui/components/FormWidgets/CountryPicker.tsx
+++ apps/demo/src/Views/Ui/components/FormWidgets/CountryPicker.tsx
@@ -1,13 +1,17 @@
 import { CountryPicker } from "@prefabs.tech/react-ui";
+import { useState } from "react";
 
 export const CountryPickerDemo = () => {
+  const [selectedCountry, setSelectedCountry] = useState<string>("NP");
   return (
     <>
       <CountryPicker
         name="country"
         locale="fr"
-        value="NP"
-        onChange={() => {}}
+        value={selectedCountry}
+        onChange={(value: string) => {
+          setSelectedCountry(value as string);
+        }}
       />
     </>
   );
