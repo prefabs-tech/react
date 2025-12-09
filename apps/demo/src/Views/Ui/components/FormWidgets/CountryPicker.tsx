01ab68d872029a71586f43291f58118ce27df609
Utsav Luintel <utsav.luintel@dzangolab.com>
2025-12-09 12:52:58 +0545

    feat(ui/countryPicker): add multiple select

--- apps/demo/src/Views/Ui/components/FormWidgets/CountryPicker.tsx
+++ apps/demo/src/Views/Ui/components/FormWidgets/CountryPicker.tsx
@@ -2,15 +2,16 @@ import { CountryPicker } from "@prefabs.tech/react-ui";
 import { useState } from "react";
 
 export const CountryPickerDemo = () => {
-  const [selectedCountry, setSelectedCountry] = useState<string>("NP");
+  const [selectedCountry, setSelectedCountry] = useState<string[]>(["NP"]);
   return (
     <>
       <CountryPicker
         name="country"
         locale="fr"
+        multiple={true}
         value={selectedCountry}
-        onChange={(value: string) => {
-          setSelectedCountry(value as string);
+        onChange={(value: string[]) => {
+          setSelectedCountry(value as string[]);
         }}
       />
     </>
