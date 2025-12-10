bd63d2fe8563437ce105b944ab8257b99570b96d
Utsav Luintel <utsav.luintel@dzangolab.com>
2025-12-10 13:14:34 +0545

    feat: add custom data support and update demo

--- packages/ui/src/FormWidgets/CountryPicker/index.tsx
+++ packages/ui/src/FormWidgets/CountryPicker/index.tsx
@@ -12,26 +12,60 @@ export interface Country {
   };
 }
 
+type CountryInput = {
+  code: string;
+  i18n: Partial<Country["i18n"]>;
+};
+
 export type CountryPickerProperties<T> = Omit<
   ISelectProperties<T>,
   "options"
 > & {
-  data?: Country[];
+  data?: CountryInput[];
   locale?: "en" | "fr" | "th";
 };
 
 export const CountryPicker = <T extends string | number>({
-  data = countriesList as Country[],
+  data = [],
   locale = "en",
   ...properties
 }: CountryPickerProperties<T>) => {
-  const countries = data.map((item) => ({
-    value: item.code as unknown as T,
-    label: `${item.i18n[locale] || item.i18n.en}`,
-    ...item,
-  }));
+  let updatedcountriesList = countriesList as Country[];
 
-  return (
-    <Select {...(properties as ISelectProperties<T>)} options={countries} />
-  );
+  if (data && data.length > 0) {
+    const countryMap = new Map<string, Country | CountryInput>(
+      updatedcountriesList.map((country) => [country.code, country]),
+    );
+
+    data.forEach((item) => {
+      const existing = countryMap.get(item.code) as Country;
+
+      if (existing) {
+        countryMap.set(item.code, {
+          ...existing,
+          ...item,
+          i18n: {
+            ...existing.i18n,
+            ...item.i18n,
+          },
+        });
+      } else {
+        countryMap.set(item.code, item);
+      }
+    });
+
+    updatedcountriesList = Array.from(countryMap.values()) as Country[];
+  }
+
+  const options = updatedcountriesList.map((item) => {
+    const label = item.i18n?.[locale] || item.i18n?.en;
+
+    return {
+      value: item.code as unknown as T,
+      label,
+      ...item,
+    };
+  });
+
+  return <Select {...(properties as ISelectProperties<T>)} options={options} />;
 };
