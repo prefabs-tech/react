1e4cb4b52535a81d796604cd00af655a3f8c3388
Utsav789 <99395965+Utsav789@users.noreply.github.com>
2025-12-11 15:15:21 +0545

    feat(demo/CountryPicker): add translations and properties table (#1647)
    * feat(ui/CountryPicker): add translations and properties table
    
    * fix: fix description and title
    
    * fix: labels and placeholders
    
    * fix: remove and improve labels
    
    * fix: indentation and alphabetical order

--- apps/demo/src/Views/Ui/components/FormWidgets/CountryPicker.tsx
+++ apps/demo/src/Views/Ui/components/FormWidgets/CountryPicker.tsx
@@ -1,5 +1,10 @@
 import { useTranslation } from "@prefabs.tech/react-i18n";
-import { CountryPicker, Page, Button } from "@prefabs.tech/react-ui";
+import {
+  CountryPicker,
+  Page,
+  Button,
+  TDataTable,
+} from "@prefabs.tech/react-ui";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -9,13 +14,72 @@ export const CountryPickerDemo = () => {
   const [t] = useTranslation("ui");
   const navigate = useNavigate();
 
+  const data = [
+    {
+      id: 1,
+      prop: "label",
+      type: "string",
+      default: "-",
+      description: t("countryPicker.propertiesDescription.label"),
+    },
+    {
+      id: 2,
+      prop: "name",
+      type: "string",
+      default: "-",
+      description: t("countryPicker.propertiesDescription.name"),
+    },
+    {
+      id: 3,
+      prop: "locale",
+      type: "string",
+      default: '"en"',
+      description: t("countryPicker.propertiesDescription.locale"),
+    },
+    {
+      id: 4,
+      prop: "value",
+      type: "string | string[]",
+      default: "-",
+      description: t("countryPicker.propertiesDescription.value"),
+    },
+    {
+      id: 5,
+      prop: "onChange",
+      type: "(value: string | string[]) => void",
+      default: "-",
+      description: t("countryPicker.propertiesDescription.onChange"),
+    },
+    {
+      id: 6,
+      prop: "placeholder",
+      type: "string",
+      default: "-",
+      description: t("countryPicker.propertiesDescription.placeholder"),
+    },
+    {
+      id: 7,
+      prop: "multiple",
+      type: "boolean",
+      default: "false",
+      description: t("countryPicker.propertiesDescription.multiple"),
+    },
+    {
+      id: 8,
+      prop: "data",
+      type: "CountryData[]",
+      default: "[]",
+      description: t("countryPicker.propertiesDescription.data"),
+    },
+  ];
+
   const [singleSelectValue, setSingleSelectValue] = useState<string>("");
-  const [multiSelectValue, setMultiSelectValue] = useState<string[]>([]);
+  const [multipleSelectValue, setMultipleSelectValue] = useState<string[]>([]);
   const [customDataValue, setCustomDataValue] = useState<string>("");
 
   return (
     <Page
-      title="Country Picker"
+      title={t("countryPicker.title")}
       toolbar={
         <Button
           label={t("buttons.back")}
@@ -30,73 +94,69 @@ export const CountryPickerDemo = () => {
         <CodeBlock exampleCode='import { CountryPicker } from "@prefabs.tech/react-ui"' />
       </Section>
 
-      <Section title="Basic (single select)">
+      <Section title={t("countryPicker.basic")}>
         <CountryPicker
-          label="Select Country"
+          label={t("countryPicker.labels.single")}
           name="countryPickerSingle"
           locale="fr"
           value={singleSelectValue}
           onChange={(value: string) => setSingleSelectValue(value)}
-          placeholder="Select a country..."
+          placeholder={t("countryPicker.placeholders.single")}
         />
         <CodeBlock
           exampleCode='
 const [singleSelectValue, setSingleSelectValue] = useState<string>("");
 
 <CountryPicker
-  label="Select Country"
+  label={t("countryPicker.labels.single")}
   name="countryPickerSingle"
   locale="fr"
   value={singleSelectValue}
   onChange={(value: string) => setSingleSelectValue(value)}
-  placeholder="Select a country..."
+  placeholder={t("countryPicker.placeholders.single")}
 />'
         />
       </Section>
 
-      <Section title="Multiple selection">
+      <Section title={t("countryPicker.multipleSelect")}>
         <CountryPicker
-          label="Select Countries"
-          name="countryPickerMulti"
+          label={t("countryPicker.labels.multiple")}
+          name="countryPickerMultiple"
           locale="en"
           multiple={true}
-          value={multiSelectValue}
-          onChange={(value: string[]) => setMultiSelectValue(value)}
-          placeholder="Select countries..."
+          value={multipleSelectValue}
+          onChange={(value: string[]) => setMultipleSelectValue(value)}
+          placeholder={t("countryPicker.placeholders.multiple")}
         />
         <CodeBlock
           exampleCode='
-const [multiSelectValue, setMultiSelectValue] = useState<string[]>([]);
+const [multipleSelectValue, setMultipleSelectValue] = useState<string[]>([]);
 
 <CountryPicker
-  label="Select Countries"
-  name="countryPickerMulti"
+  label={t("countryPicker.labels.multiple")}
+  name="countryPickerMultiple"
   locale="en"
   multiple={true}
-  value={multiSelectValue}
-  onChange={(value: string[]) => setMultiSelectValue(value)}
-  placeholder="Select countries..."
+  value={multipleSelectValue}
+  onChange={(value: string[]) => setMultipleSelectValue(value)}
+  placeholder={t("countryPicker.placeholders.multiple")}
 />'
         />
       </Section>
-      <Section title="Custom data (overwrite & create)">
-        <p>
-          This example overwrites "France" (FR) with a custom label and adds a
-          new entry for "Mars" (XX).
-        </p>
+      <Section title={t("countryPicker.customData")}>
         <CountryPicker
-          label="Select Country (with Custom Data)"
+          label={t("countryPicker.labels.single")}
           name="countryPickerCustom"
           locale="en"
           value={customDataValue}
           onChange={(value: string) => setCustomDataValue(value)}
-          placeholder="Look for Mars or Custom France..."
+          placeholder={t("countryPicker.placeholders.single")}
           data={[
             {
               code: "FR",
               i18n: {
-                en: "France (Custom Overwrite)",
-                fr: "La France (Custom)",
+                en: "France (fr)",
+                fr: "La France (fr)",
               },
             },
             {
@@ -110,14 +170,18 @@ const [multiSelectValue, setMultiSelectValue] = useState<string[]>([]);
 const [customDataValue, setCustomDataValue] = useState<string>("");
 
 <CountryPicker
-  label="Select Country"
+  label={t("countryPicker.labels.custom")}
   locale="en"
   value={customDataValue}
   onChange={(value) => setCustomDataValue(value)}
+  placeholder={t("countryPicker.placeholders.custom")}
   data={[
     { 
       code: "FR", 
-      i18n: { en: "France (Custom Overwrite)" } 
+      i18n: { 
+        en: "France (fr)",
+        fr: "La France (fr)",
+       } 
     },
     { 
       code: "XX", 
@@ -125,6 +189,35 @@ const [customDataValue, setCustomDataValue] = useState<string>("");
     }
   ]}
 />'
+        />
+      </Section>
+      <Section
+        title={t("headers.propertiesValue", {
+          value: "CountryPicker",
+        })}
+      >
+        <TDataTable
+          columns={[
+            {
+              accessorKey: "prop",
+              header: "Properties",
+            },
+            {
+              accessorKey: "type",
+              header: "Type",
+            },
+            {
+              accessorKey: "default",
+              header: "Default",
+            },
+            {
+              accessorKey: "description",
+              header: "Description",
+            },
+          ]}
+          data={data}
+          paginated={false}
+          persistState={false}
         />
       </Section>
     </Page>
