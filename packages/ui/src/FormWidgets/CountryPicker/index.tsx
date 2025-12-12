399e4c5c083fade9ee863e0160bb0beb5f01e004
Utsav789 <99395965+Utsav789@users.noreply.github.com>
2025-12-12 11:41:21 +0545

    feat(ui/CountryPicker): add include prop and update demo (#1648)
    * feat(ui/CountryPicker): add include and update demo
    
    * perf: use useMemo for rendering optimization

--- packages/ui/src/FormWidgets/CountryPicker/index.tsx
+++ packages/ui/src/FormWidgets/CountryPicker/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import countriesList from "./countries.json";
 import { Select, ISelectProperties } from "../Select";
@@ -22,50 +22,60 @@ export type CountryPickerProperties<T> = Omit<
   "options"
 > & {
   data?: CountryData[];
+  include?: string[];
   locale?: "en" | "fr" | "th";
 };
 
 export const CountryPicker = <T extends string | number>({
   data,
+  include,
   locale = "en",
   ...properties
 }: CountryPickerProperties<T>) => {
   let updatedCountriesList = countriesList as Country[];
 
-  if (data && data.length > 0) {
-    const countryMap = new Map<string, Country | CountryData>(
-      updatedCountriesList.map((country) => [country.code, country]),
-    );
+  const options = useMemo(() => {
+    if (data && data.length > 0) {
+      const countryMap = new Map<string, Country | CountryData>(
+        updatedCountriesList.map((country) => [country.code, country]),
+      );
 
-    data.forEach((item) => {
-      const existing = countryMap.get(item.code) as Country;
+      data.forEach((item) => {
+        const existing = countryMap.get(item.code) as Country;
 
-      if (existing) {
-        countryMap.set(item.code, {
-          ...existing,
-          ...item,
-          i18n: {
-            ...existing.i18n,
-            ...item.i18n,
-          },
-        });
-      } else {
-        countryMap.set(item.code, item);
-      }
+        if (existing) {
+          countryMap.set(item.code, {
+            ...existing,
+            ...item,
+            i18n: {
+              ...existing.i18n,
+              ...item.i18n,
+            },
+          });
+        } else {
+          countryMap.set(item.code, item);
+        }
+      });
+
+      updatedCountriesList = Array.from(countryMap.values()) as Country[];
+    }
+
+    if (include && include.length > 0) {
+      updatedCountriesList = updatedCountriesList.filter((country) =>
+        include.includes(country.code),
+      );
+    }
+
+    return updatedCountriesList.map((item) => {
+      const label = item.i18n?.[locale] || item.i18n?.en;
+
+      return {
+        value: item.code as unknown as T,
+        label,
+        ...item,
+      };
     });
-
-    updatedCountriesList = Array.from(countryMap.values()) as Country[];
-  }
-
-  const options = updatedCountriesList.map((item) => {
-    const label = item.i18n?.[locale] || item.i18n?.en;
-
-    return {
-      value: item.code as unknown as T,
-      label,
-      ...item,
-    };
-  });
+  }, [data, include, locale]);
 
   return <Select {...(properties as ISelectProperties<T>)} options={options} />;
 };
