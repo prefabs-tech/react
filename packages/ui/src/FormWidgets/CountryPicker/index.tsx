ad265f491279128ea3a9a78e2dc62b63f3622ed4
Utsav Luintel <utsav.luintel@dzangolab.com>
2026-01-13 10:04:49 +0545

    refactor(ui/countryPicker): remove default groups

--- packages/ui/src/FormWidgets/CountryPicker/index.tsx
+++ packages/ui/src/FormWidgets/CountryPicker/index.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useMemo } from "react";
 
 import { getFallbackTranslation } from "../../utils/CountryPicker";
 import { Select, ISelectProperties } from "../Select";
-import defaultGroups from "./groups.json";
 
 import type { Option, GroupedOption as OptionGroup } from "../Select";
 
@@ -34,7 +33,6 @@ export type CountryPickerProperties<T> = Omit<
   locale?: string;
   locales?: Locales;
 };
-export { defaultGroups };
 
 const getBaseOptions = <T,>(
   exclude: string[] | undefined,
