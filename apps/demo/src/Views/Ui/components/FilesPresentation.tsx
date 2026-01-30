d0a9748da0efb7591af3b239473f0b525f28af52
Utsav Luintel <utsav.luintel@dzangolab.com>
2026-01-30 13:30:52 +0545

    feat(demo/files-presentation): update demo and add props table

--- apps/demo/src/Views/Ui/components/FilesPresentation.tsx
+++ apps/demo/src/Views/Ui/components/FilesPresentation.tsx
@@ -1,12 +1,69 @@
 import { useTranslation } from "@prefabs.tech/react-i18n";
-import { Button, FilesPresentation, Page } from "@prefabs.tech/react-ui";
+import {
+  Button,
+  FilesPresentation,
+  Page,
+  TDataTable,
+} from "@prefabs.tech/react-ui";
 import { useNavigate } from "react-router-dom";
 
-import { Section } from "../../../components/Demo";
+import { CodeBlock, Section } from "../../../components/Demo";
 
 export const FilesPresentationDemo = () => {
   const { i18n, t } = useTranslation("ui");
   const navigate = useNavigate();
+
+  const data = [
+    {
+      id: 1,
+      prop: "files",
+      type: "IFile[]",
+      default: "[]",
+      description: t("filesPresentation.propertiesDescription.files"),
+    },
+    {
+      id: 2,
+      prop: "listProps",
+      type: "Partial<FilesListProps>",
+      default: "undefined",
+      description: t("filesPresentation.propertiesDescription.listProps"),
+    },
+    {
+      id: 3,
+      prop: "locale",
+      type: "string",
+      default: "undefined",
+      description: t("filesPresentation.propertiesDescription.locale"),
+    },
+    {
+      id: 4,
+      prop: "onFile[Action]",
+      type: "(file: IFile) => void",
+      default: "undefined",
+      description: t("filesPresentation.propertiesDescription.actions"),
+    },
+    {
+      id: 5,
+      prop: "presentation",
+      type: '"list" | "table"',
+      default: '"list"',
+      description: t("filesPresentation.propertiesDescription.presentation"),
+    },
+    {
+      id: 6,
+      prop: "tableProps",
+      type: "Partial<FilesTableProps>",
+      default: "undefined",
+      description: t("filesPresentation.propertiesDescription.tableProps"),
+    },
+    {
+      id: 7,
+      prop: "visibleFileDetails",
+      type: "string[]",
+      default: "undefined",
+      description: t("filesPresentation.propertiesDescription.visibleDetails"),
+    },
+  ];
 
   return (
     <Page
@@ -20,7 +77,8 @@ export const FilesPresentationDemo = () => {
         />
       }
     >
-      <Section>
+      <Section title={t("headers.usage")}>
+        <p>{t("common.usage", { component: "FilesPresentation" })}</p>
         <FilesPresentation
           locale={i18n?.language}
           visibleFileDetails={[
@@ -79,6 +137,80 @@ export const FilesPresentationDemo = () => {
               lastDownloadedAt: new Date("2025-03-08").getTime(),
             },
           ]}
+        />
+        <CodeBlock
+          exampleCode={`import { FilesPresentation } from "@prefabs.tech/react-ui";
+
+const myFiles = [
+  { id: 1, originalFileName: "doc.pdf", size: 1024, ... },
+  { id: 2, originalFileName: "img.png", size: 2048, ... }
+];
+
+<FilesPresentation
+  files={myFiles}
+  presentation="table"
+  locale="en"
+  onFileDownload={(file) => console.log(file)}
+/>
+
+<FilesPresentation
+  files={myFiles}
+  presentation="list"
+  locale="en"
+  listProps={{ showFileThumbnail: true }}
+/>`}
+        />
+      </Section>
+
+      <Section
+        title={t("headers.propertiesValue", {
+          value: "FilesPresentation",
+        })}
+      >
+        <TDataTable
+          columns={[
+            {
+              accessorKey: "prop",
+              header: "Properties",
+            },
+            {
+              accessorKey: "type",
+              header: "Type",
+            },
+            {
+              accessorKey: "default",
+              header: "Default",
+            },
+            {
+              accessorKey: "description",
+              header: "Description",
+            },
+          ]}
+          data={data}
+          paginated={false}
+          persistState={false}
+        />
+      </Section>
+
+      <Section
+        title={t("filesPresentation.typeDefinitions", "Type Definitions")}
+      >
+        <CodeBlock
+          exampleCode={`interface IFilesPresentationProperties {
+  presentation?: "list" | "table";
+  files: Array<IFile>;
+  locale?: string;
+  messages?: TableMessages;
+  onFileArchive?: (file: IFile) => void;
+  onFileDownload?: (file: IFile) => void;
+  onFileDelete?: (file: IFile) => void;
+  onEditDescription?: (file: IFile) => void;
+  onFileShare?: (file: IFile) => void;
+  onFileView?: (file: IFile) => void;
+  visibleFileDetails?: string[];
+  listProps?: Partial<ComponentProps<typeof FilesList>>;
+  tableProps?: Partial<ComponentProps<typeof FilesTable>>;
+}`}
         />
       </Section>
     </Page>
