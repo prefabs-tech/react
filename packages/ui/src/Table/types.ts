9363c49da806f94e06d2afee39a507e74d8f35c3
Kabin Khand Thakuri <kabin@dzangolab.com>
2026-07-01 14:14:19 +0545

    feat(table): add support to limit date  for date range filter (#1726)

--- packages/ui/src/Table/types.ts
+++ packages/ui/src/Table/types.ts
@@ -44,6 +44,8 @@ declare module "@tanstack/react-table" {
 
   /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
   interface ColumnMeta<TData extends RowData, TValue> {
+    dateFilterMax?: Date;
+    dateFilterMin?: Date;
     filterOptions?: FilterOption[];
     filterVariant?: TFilterVariant;
     rangeFilterMax?: number;
